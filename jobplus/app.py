a05ead50fb9a00b02e1babf567194f001ec5c53e
zkqiang <zkqiang@126.com>
2018-02-23 19:37:22 +0800

    添加项目基本结构

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,11 +1,37 @@
-from flask import Flask,render_template
+from flask import Flask
 from jobplus.config import configs
-from jobplus.models import db
+from jobplus.models import db, User
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+def register_extensions(app):
+    db.init_app(app)
+    Migrate(app, db)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def user_loader(id):
+        return User.query.get(id)
+
+    login_manager.login_view = 'front.login'
+
+
+def register_blueprints(app):
+    from .handlers import front, admin, user, company, resume, job
+    app.register_blueprint(front)
+    app.register_blueprint(user)
+    app.register_blueprint(admin)
+    app.register_blueprint(company)
+    app.register_blueprint(resume)
+    app.register_blueprint(job)
+
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
-    db.init_app(app)
-
+    register_extensions(app)
+    register_blueprints(app)
 
     return app
--- jobplus/config.py
+++ jobplus/config.py
@@ -1,19 +1,22 @@
 class BaseConfig(object):
-    SECRET_KEY='make sure to set a very confidential key to carnivore'
+    SECRET_KEY = 'makesure to set a very secret key'
+
 
 class DevelopmentConfig(BaseConfig):
-    DEBUG=True
-    SQLALCHEMY_DATABASE_URI='mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
+
 
 class ProductionConfig(BaseConfig):
     pass
 
+
 class TestingConfig(BaseConfig):
     pass
 
-configs={
-    'development':DevelopmentConfig,
-    'production':ProductionConfig,
-    'testing':TestingConfig
+
+configs = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
 }
-    
